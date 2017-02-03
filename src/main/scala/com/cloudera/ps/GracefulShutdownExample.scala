c62939ad21b75b9feffe6b9cdda2948beedc789d
Lan Jiang <ljiang2@gmail.com>
2017-02-02 18:55:42 -0800

    Add HDFS shutdown marker check. Not checking incoming string STOP any more

--- src/main/scala/com/cloudera/ps/GracefulShutdownExample.scala
+++ src/main/scala/com/cloudera/ps/GracefulShutdownExample.scala
@@ -3,12 +3,14 @@ package com.cloudera.ps
 /**
   * Created by ljiang on 12/13/16.
   */
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark._
-import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming._
 
 
 object GracefulShutdownExample {
+  val shutdownMarker = "hdfs:///tmp/shutdownmarker"
   var stopFlag:Boolean = false
   def main(args: Array[String]) {
     val conf = new SparkConf().setAppName("SparkStreamingGracefulShutdown")
@@ -16,12 +18,10 @@ object GracefulShutdownExample {
     val ssc = new StreamingContext(conf, Seconds(5))
     val lines = ssc.socketTextStream("ljiang-spark-1.vpc.cloudera.com", 9999)
     lines.print()
-    lines.foreachRDD(rdd => checkRdd(rdd))
     ssc.start()
     val checkIntervalMillis = 10000
     var isStopped = false
 
-    println("before while")
     while (! isStopped) {
       println("calling awaitTerminationOrTimeout")
       isStopped = ssc.awaitTerminationOrTimeout(checkIntervalMillis)
@@ -29,6 +29,7 @@ object GracefulShutdownExample {
         println("confirmed! The streaming context is stopped. Exiting application...")
       else
         println("Streaming App is still running. Timeout...")
+      checkShutdownMarker
       if (!isStopped && stopFlag) {
         println("stopping ssc right now")
         ssc.stop(true, true)
@@ -37,9 +38,11 @@ object GracefulShutdownExample {
     }
   }
 
-  def checkRdd(rdd: RDD[String]) = {
-    val strings = rdd.collect()
-    if (!stopFlag)
-      stopFlag = strings.contains("STOP")
+  def checkShutdownMarker = {
+    if (!stopFlag) {
+      val fs = FileSystem.get(new Configuration())
+      stopFlag = fs.exists(new Path(shutdownMarker))
+    }
+
   }
 }
